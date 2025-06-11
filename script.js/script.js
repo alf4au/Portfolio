0dd13fb2d8093d7a81fc8737e72d88a8b8877831
alf4au <55000448+alf4au@users.noreply.github.com>
2025-06-11 23:45:52 +0200

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+  console.log('Portfolio website loaded!');
+  // alert('Welcome to my portfolio website!'); // You can uncomment this if you want a popup
+});
